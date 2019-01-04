266388b23c8c50588c8b11c2082e1b50f99a4f9b
TuanPham303 <phamcongtuan303@gmail.com>
2019-01-04 09:04:53 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { BlogEditor } from './components';
 import './App.css';
@@ -8,5 +9,20 @@ const App = () => (
   </div>
 );
 
+=======
+import React, { Component } from 'react';
+import { BlogEditor } from './components';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BlogEditor/>
+      </div>
+    );
+  }
+}
+>>>>>>> first commit
 
 export default App;
