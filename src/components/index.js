a2e3506fa6e047d35af3ccfd762bd00706c2a338
TuanPham303 <phamcongtuan303@gmail.com>
2019-01-09 09:02:07 -0800

    setup ẽport structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,3 @@
-import BlogEditor from './BlogEditor/BlogEditor';
-import EdittingIcon from './EdittingIcon/EdittingIcon';
-import EdittingPanel from './EdittingPanel/EdittingPanel';
-
-export {
-  BlogEditor,
-  EdittingIcon,
-  EdittingPanel,
-};
+export { default as BlogEditor } from './BlogEditor';
+export { default as EdittingIcon } from './EdittingIcon';
+export { default as EdittingPanel } from './EdittingPanel';
